1452c75f6cf725045b09ca8e5e212dd3f367e680
Sebastian Bachmann <github@sebastianbachmann.de>
2024-06-12 23:09:54 +0200

    chore: removed some components and added a table

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,90 @@
 import { Button } from '@/components/shared/ui/button';
 import Header from '@/components/shared/Header';
 import { LandingPrimaryImageCtaSection } from '@/components/landing/cta/LandingPrimaryCta';
-import { LandingProductHuntAward } from '@/components/landing/social-proof/LandingProductHuntAward';
-import { LandingSocialProof } from '@/components/landing/social-proof/LandingSocialProof';
-import { LandingDiscount } from '@/components/landing/discount/LandingDiscount';
-import LatestArticles from '@/components/blog/LatestArticles';
-import { ComponentDemo } from 'demo/demo';
-import { LandingProductFeature } from '@/components/landing/LandingProductFeature';
-import { LandingProductFeatureKeyPoints } from '@/components/landing/LandingProductFeatureKeyPoints';
-import { LandingSaleCtaSection } from '@/components/landing/cta/LandingSaleCta';
-import { LandingTestimonialGrid } from '@/components/landing/testimonial/LandingTestimonialGrid';
-import { LandingBandSection } from '@/components/landing/LandingBand';
-import { LandingTestimonialReadMoreWrapper } from '@/components/landing/testimonial/LandingTestimonialReadMoreWrapper';
-import { LandingFeatureList } from '@/components/landing/feature/LandingFeatureList';
-import { LandingFaqCollapsibleSection } from '@/components/landing/LandingFaqCollapsible';
-import { LandingSocialProofBand } from '@/components/landing/social-proof/LandingSocialProofBand';
-import { LandingSocialProofBandItem } from '@/components/landing/social-proof/LandingSocialProofBandItem';
 
 import {
-  ChromeIcon,
-  FigmaIcon,
-  FramerIcon,
-  GithubIcon,
-  LayersIcon,
-  LightbulbIcon,
-  LineChartIcon,
-  SparklesIcon,
-  ThumbsUpIcon,
-  ZapIcon,
-} from 'lucide-react';
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/shared/ui/table';
+
+import LatestArticles from '@/components/blog/LatestArticles';
+
+const animals = [
+  {
+    type: 'Rotwild',
+    class: [
+      'Kaelber\n\n',
+      'Schmalspießer\n\n',
+      'Schmaltiere\n\n',
+      'Alttiere und Hirsche\n\n',
+    ],
+    nationwide: [
+      '01.08.- 28.02.',
+      '01.06.- 28.02.\n\n',
+      '01.06.- 31.01.\n\n',
+      '01.08.- 31.01.\n\n',
+    ],
+    hesse: [
+      '01.08.- 31.01.\n\n',
+      '01.04.- 31.05 & 01.08.-31.01\n\n',
+      '01.04.- 31.05 & 01.08.-31.01\n\n',
+      '---\n\n',
+    ],
+    notes: 'tbd',
+  },
+  {
+    type: 'Dam- / Sikawild',
+    class: 'tbd',
+    nationwide: 'tbd',
+    hesse: 'tbd',
+    notes: 'tbd',
+  },
+  {
+    type: 'Rehwild',
+    class: 'tbd',
+    nationwide: 'tbd',
+    paymentMethod: 'tbd',
+  },
+  {
+    type: 'Gamswild',
+    class: 'tbd',
+    nationwide: 'tbd',
+    paymentMethod: 'tbd',
+  },
+  {
+    type: 'Muffelwild',
+    class: 'tbd',
+    nationwide: 'tbd',
+    paymentMethod: 'tbd',
+  },
+  {
+    type: 'Schwarzwild',
+    class: 'tbd',
+    nationwide: 'tbd',
+    paymentMethod: 'tbd',
+  },
+  {
+    type: 'Feldhasen',
+    class: 'tbd',
+    nationwide: 'tbd',
+    paymentMethod: 'tbd',
+  },
+];
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full items-center fancy-overlay">
       <Header className="mb-0 lg:mb-0" />
-
       <LandingPrimaryImageCtaSection
         title="Jagd- und Schonzeiten in Hessen"
         description="Die Jagd- und Schonzeiten in Hessen als PDF Download und Kalenderdatei."
-        imageSrc="/static/images/product-sample.webp"
+        imageSrc="/static/images/irina-iriser-pBs9bFqR0D8-unsplash.jpg"
         withBackground
         withBackgroundGlow
       >
@@ -49,298 +95,31 @@ export default function Home() {
         <Button size="xl" asChild variant="outlinePrimary">
           <a href="/termine">Zu den Terminen</a>
         </Button>
-
-        <LandingSocialProof
-          className="w-full mt-12"
-          showRating
-          numberOfUsers={99}
-          suffixText="happy users"
-          avatarItems={[
-            {
-              imageSrc: 'https://picsum.photos/id/64/100/100',
-              name: 'John Doe',
-            },
-            {
-              imageSrc: 'https://picsum.photos/id/65/100/100',
-              name: 'Jane Doe',
-            },
-            {
-              imageSrc: 'https://picsum.photos/id/669/100/100',
-              name: 'Alice Doe',
-            },
-          ]}
-        />
       </LandingPrimaryImageCtaSection>
-
-      <LandingProductFeature
-        title="Craft Unique Solutions with Ease"
-        descriptionComponent={
-          <>
-            <LandingProductFeatureKeyPoints
-              keyPoints={[
-                {
-                  title: 'Intuitive Interface',
-                  description:
-                    'Design and customize your app easily with our simple drag-and-drop interface.',
-                },
-                {
-                  title: 'Seamless Integration',
-                  description:
-                    'Connect your app with other tools effortlessly for a smoother workflow.',
-                },
-                {
-                  title: 'Smart Analytics',
-                  description:
-                    'Gain valuable insights into user behavior and trends with our advanced analytics tools.',
-                },
-              ]}
-            />
-
-            <Button asChild>
-              <a href="/jetzt-kaufen">Jetzt kaufen!</a>
-            </Button>
-
-            <p className="text-sm">
-              7 day free trial, no credit card required.
-            </p>
-          </>
-        }
-        imageSrc="/static/images/backdrop-19.webp"
-        imageAlt="Screenshot of the product"
-        imagePosition="left"
-        imagePerspective="none"
-      />
-
-      <LandingProductFeature
-        title="Smart Task Prioritization"
-        descriptionComponent={
-          <>
-            <p>
-              Our AI-powered task prioritization feature automatically organizes
-              your to-do list based on deadlines, importance, and your work
-              patterns, ensuring you focus on the most critical tasks first.
-            </p>
-
-            <LandingProductFeatureKeyPoints
-              keyPoints={[
-                {
-                  title: 'Rock-Solid Security',
-                  description:
-                    'Rest assured, your data is safe with our top-notch security measures.',
-                },
-                {
-                  title: 'Automatic Updates',
-                  description:
-                    'Never miss out on the latest features - our app updates itself automatically!',
-                },
-                {
-                  title: 'Scalability on Demand',
-                  description:
-                    'Grow your app along with your business needs, effortlessly expanding to meet demand.',
-                },
-              ]}
-            />
-
-            <Button asChild variant="outlinePrimary">
-              <a href="/termine">Zu den Terminen</a>
-            </Button>
-
-            <p className="text-sm">Get started with our free tier.</p>
-          </>
-        }
-        imageSrc="/static/images/backdrop-20.webp"
-        imageAlt="Screenshot of the product"
-        imagePosition="right"
-        imagePerspective="none"
-        withBackground
-        withBackgroundGlow
-        variant="secondary"
-        backgroundGlowVariant="secondary"
-      />
-
-      <LandingProductFeature
-        title="One-click API Integration"
-        descriptionComponent={
-          <>
-            <p>
-              With our API integration capability, seamlessly connect our app
-              with other software and systems in your ecosystem, enabling data
-              exchange, automation, and enhanced functionality.
-            </p>
-
-            <Button asChild variant="outlinePrimary">
-              <a href="/termine">Zu den Terminen</a>
-            </Button>
-
-            <p className="text-sm">First month is on us.</p>
-          </>
-        }
-        imageSrc="/static/images/backdrop-5.webp"
-        imageAlt="Screenshot of the product"
-        imagePosition="left"
-        imagePerspective="none"
-        variant="secondary"
-      />
-
-      <LandingProductFeature
-        title="Supercharge Your Efficiency!"
-        descriptionComponent={
-          <>
-            Revolutionize your workflow and accomplish more with our amazing AI
-            app. It's fast, user-friendly, and beautifully designed!
-            <Button asChild variant="outlinePrimary">
-              <a href="/termine">Zu den Terminen</a>
-            </Button>
-          </>
-        }
-        withBackground
-        variant="secondary"
-        imageSrc="/static/images/product-sample.webp"
-        imageAlt="Screenshot of the product"
-        imagePosition="center"
-        textPosition="center"
-      />
-
-      <LandingSaleCtaSection
-        title="Unleash Your Potential!"
-        description="Elevate your business with our cutting-edge AI app. Boost efficiency and drive revenue to new heights!"
-        ctaHref={'#'}
-        ctaLabel={'Pre-order now'}
-        withBackgroundGlow
-      />
-
-      <LandingTestimonialReadMoreWrapper size="md">
-        <LandingTestimonialGrid
-          title="Hear It from Our Users"
-          description="Discover what our happy customers have to say about their experience with our AI app:"
-          testimonialItems={[
-            {
-              name: 'John Smith',
-              text: 'This app transformed our operations, boosting productivity like never before. Highly customizable and incredibly efficient!',
-              handle: '@john_smith',
-              imageSrc: 'https://picsum.photos/id/64/100/100',
-            },
-            {
-              name: 'Emily Johnson',
-              text: "Even with minimal tech knowledge, I could create my own app from scratch. It's empowering to have full control!",
-              handle: '@emily_johnson',
-              imageSrc: 'https://picsum.photos/id/65/100/100',
-            },
-            {
-              name: 'David Rodriguez',
-              text: 'Thanks to the analytics tools, we identified bottlenecks and saved significantly on costs. Truly impressive!',
-              handle: '@david_rodriguez',
-              imageSrc: 'https://picsum.photos/id/669/100/100',
-              featured: true,
-            },
-            {
-              name: 'Mandy',
-              text: 'Excellent product!',
-              handle: '@mandy',
-              imageSrc: 'https://picsum.photos/id/829/100/100',
-            },
-            {
-              name: 'Alex',
-              text: 'Can easily recommend!',
-              handle: '@alex',
-              imageSrc: 'https://picsum.photos/100/100.webp?random=2',
-            },
-            {
-              name: 'Sam',
-              text: 'I am very happy with the results.',
-              handle: '@sam',
-              imageSrc: 'https://picsum.photos/100/100.webp?random=3',
-            },
-          ]}
-          withBackgroundGlow
-          withBackground
-        />
-      </LandingTestimonialReadMoreWrapper>
-
-      <LandingFeatureList
-        title="Awesome Features Await!"
-        description="Explore the fantastic features of our AI app:"
-        featureItems={[
-          {
-            title: 'Intuitive Interface',
-            description:
-              'Design and customize your app easily with our simple drag-and-drop interface.',
-            icon: <LayersIcon />,
-          },
-          {
-            title: 'Seamless Integration',
-            description:
-              'Connect your app with other tools effortlessly for a smoother workflow.',
-            icon: <LineChartIcon />,
-          },
-          {
-            title: 'Smart Analytics',
-            description:
-              'Gain valuable insights into user behavior and trends with our advanced analytics tools.',
-            icon: <SparklesIcon />,
-          },
-          {
-            title: 'Rock-Solid Security',
-            description:
-              'Rest assured, your data is safe with our top-notch security measures.',
-            icon: <LightbulbIcon />,
-          },
-          {
-            title: 'Automatic Updates',
-            description:
-              'Never miss out on the latest features - our app updates itself automatically!',
-            icon: <ZapIcon />,
-          },
-          {
-            title: 'Scalability on Demand',
-            description:
-              'Grow your app along with your business needs, effortlessly expanding to meet demand.',
-            icon: <ThumbsUpIcon />,
-          },
-          {
-            title: 'Intelligent Assistance',
-            description:
-              'Receive personalized recommendations and insights tailored to your workflow, helping you make informed decisions and work more efficiently.',
-            icon: <ChromeIcon />,
-          },
-          {
-            title: 'Seamless Collaboration',
-            description:
-              'Easily collaborate with team members and clients in real-time, fostering productivity and enhancing communication across projects.',
-            icon: <FigmaIcon />,
-          },
-          {
-            title: 'Advanced Customization',
-            description:
-              'Tailor your app to fit your unique requirements with extensive customization options, ensuring it aligns perfectly with your business objectives.',
-            icon: <FramerIcon />,
-          },
-        ]}
-      />
-
-      <LandingFaqCollapsibleSection
-        title="Got Questions? We've Got Answers!"
-        description="Find answers to common inquiries about our AI app:"
-        faqItems={[
-          {
-            question: 'Can I integrate my existing systems?',
-            answer:
-              'Absolutely! Our app seamlessly integrates with various other tools and systems.',
-          },
-          {
-            question: 'Do I need coding skills?',
-            answer:
-              'Nope! Our user-friendly interface empowers anyone to create and manage their own app.',
-          },
-          {
-            question: 'Is my data secure?',
-            answer:
-              'Absolutely! We take data security seriously, employing robust measures to keep your information safe.',
-          },
-        ]}
-        withBackground
-      />
-
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Wildart</TableHead>
+            <TableHead>Klasse</TableHead>
+            <TableHead>Bund</TableHead>
+            <TableHead>Hessen</TableHead>
+            <TableHead>Bemerkung</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {animals.map((animal) => (
+            <TableRow key={animal.type}>
+              <TableCell className="font-medium">{animal.type}</TableCell>
+              <TableCell>{animal.class}</TableCell>
+              <TableCell>{animal.nationwide}</TableCell>
+              <TableCell>{animal.hesse}</TableCell>
+              <TableCell>{animal.notes}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      )
       <section className="wide-container mt-12">
         <LatestArticles />
       </section>
